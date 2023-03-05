e6af054931cfeb4f549e85450a5fead8631bc039
nakudino <112789966+nakudino@users.noreply.github.com>
2023-03-05 10:59:23 -0800

    Add files via upload

---
+++ SolarGram/SolarGram/SolarGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarGramApp.swift
+//  SolarGram
+//
+//  Created by Njideka Akudinobi on 2/27/23.
+//
+
+import SwiftUI
+
+@main
+struct SolarGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ SolarGram/SolarGram/Views/ProfileView.swift
@@ -0,0 +1,60 @@
+//
+//  ProfileView.swift
+//  SolarGram
+//
+//  Created by Njideka Akudinobi on 3/3/23.
+//
+/*
+ Profile page for SolarGram user
+ */
+
+import SwiftUI
+
+enum AppeareanceStyle {
+    case dark
+    case light
+    case auto
+}
+
+struct ProfileView: View {
+    
+    private let columns = [
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+        GridItem(.flexible(minimum: 40)),
+    ]
+    
+    private let imageNames = ["taj", "colosseum", "petra", "chichan", "reef", "stonehenge"];
+    
+    var body: some View {
+        VStack{
+            Image("person")
+                .resizable()
+                .frame(width: 150, height: 150)
+                .clipShape(Circle())
+            Text("globetrotter")
+                .font(.title)
+                .fontWeight(.regular)
+                .padding()
+        ScrollView{
+            LazyVGrid(columns: columns, content: {
+                ForEach(imageNames, id: \.self) { name in
+                    Color.clear
+                        .background(Image(name)
+                            .resizable()
+                            .scaledToFill()
+                        )
+                        .aspectRatio(1, contentMode: .fill)
+                        .clipped()
+                        .contentShape(Rectangle())
+                }
+            })
+        }
+    }
+    }
+    struct ProfileView_Previews: PreviewProvider {
+        static var previews: some View {
+            ProfileView()
+        }
+    }
+}
